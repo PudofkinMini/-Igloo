42d786c37ad830327762713386c5c1416111805d
Ryan <ryan.pleava@gmail.com>
2021-02-12 19:14:01 -0500

    tools

--- bankassignment/tools.java
+++ bankassignment/tools.java
@@ -1,1 +1,37 @@
-class tools
+import java.util.*;
+import java.io.*;
+class tools{
+	public static double takeNum(){
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			String num = scan.nextLine();
+			try{
+				double attempt = Double.parseDouble(num);
+				return attempt;
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid. Please enter a number.");
+			}
+		}
+	}
+	public static String takeString(){
+		Scanner scan = new Scanner(System.in);
+		while (true){
+			String string = scan.nextLine();
+			for (int i=0; i < string.length(); i++){
+				char letter = string.charAt(i);
+				if ( (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') || letter == '-' || letter == '.'){
+					if (i==string.length()-1){
+						return string;
+					}
+				} else{
+					System.out.println("Invalid. Please enter letters.");
+					break;
+				}
+			}
+		}
+	}
+	public static void main(String[] args){
+		System.out.println(takeString());
+		System.out.println(takeNum());
+	}
+}
